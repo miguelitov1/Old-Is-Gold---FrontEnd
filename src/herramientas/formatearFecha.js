62e388897fab7f3c76db31dea8ee06d3e06179fd
miguelvisintini1@gmail.com <miguelvisintini1@gmail.com>
2021-03-11 20:55:51 +0100

    Cambio de prueba

--- src/herramientas/formatearFecha.js
+++ src/herramientas/formatearFecha.js
@@ -3,6 +3,7 @@ export const formatearFecha = (fecha) => {
     const year = fecha.substr(0, 4);
     const month = fecha.substr(5, 2);
     const day = fecha.substr(8, 2);
+
     return day + "/" + month + "/" + year;
   }
 };
